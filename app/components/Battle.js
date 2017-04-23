857d268f6b6e4352f39fece07c32c2e2f9bb1c8a
Corentin Leruth <clt@pulsar.be>
2017-04-23 23:48:42 +0200

    Forms and Encapsulation in React

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,17 +1,96 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlayerInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      username: value,
+    }));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.props.id, this.state.username);
+  }
+
+  render() {
+    const { label, onSubmit, id } = this.props;
+    const { username } = this.state;
+    return (
+      <form className="column" onSubmit={this.handleSubmit}>
+        <label className="header" htmlFor="username">
+          {label}
+        </label>
+        <input
+          id="username"
+          placeholder="github username"
+          type="text"
+          autoComplete="off"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button className="button" type="submit" disabled={!username}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
 class Battle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    this.setState(() => ({
+      [id + 'Name']: username,
+      [id + 'Image']: `https://github.com/${username}/.png?size=200`,
+    }));
+  }
+
   render() {
+    const { playerOneName, playerTwoName } = this.state;
     return (
       <div>
-        BATTLE
+        <div className="row">
+          {!playerOneName && <PlayerInput id="playerOne" label="Player One" onSubmit={this.handleSubmit} />}
+
+          {!playerTwoName && <PlayerInput id="playerTwo" label="Player Two" onSubmit={this.handleSubmit} />}
+        </div>
       </div>
     );
   }
 }
 
-Battle.propTypes = {
-
-};
+Battle.propTypes = {};
 
 export default Battle;
