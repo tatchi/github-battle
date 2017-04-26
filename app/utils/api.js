2bf06fc1180d2986995a6bad835968e9c5b3cb11
Corentin Leruth <clt@pulsar.be>
2017-04-26 19:25:43 +0200

    Axios, Promises, and the Github API

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,42 @@
 import axios from 'axios';
 
+const id = 'YOUR_CLIENT_ID';
+const sec = 'YOUR_SECRET_ID';
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+const getProfile = username => axios.get(`https://api.github.com/users/${username}${params}`).then(user => user.data);
+
+const getRepos = username =>
+  axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`).then(user => user.data);
+
+const getStarCount = repos => repos.data.reduce((count, repo) => count + repo.stargazers_count, 0);
+
+const calculateScore = (profile, repos) => {
+  const { followers } = profile;
+  const totalStars = getStarCount(repos);
+  return followers * 3 + totalStars;
+};
+
+const handleError = error => {
+  console.warn(error);
+  return null;
+};
+
+const getUserData = player =>
+  axios.all([getProfile(player), getRepos(player)]).then(data => {
+    const profile = data[0];
+    const repos = data[1];
+
+    return {
+      profile,
+      score: calculateScore(profile, repos),
+    };
+  });
+
+const sortPlayers = players => players.sort((a, b) => b.score - a.score);
+
+const battle = players => axios.all(players.map(getUserData)).then(sortPlayers).catch(handleError);
+
 const fetchPopularRepos = language => {
   const encodedURI = window.encodeURI(
     'https://api.github.com/search/repositories?q=stars:>1+language:' +
@@ -12,4 +49,5 @@ const fetchPopularRepos = language => {
 
 export default {
   fetchPopularRepos,
+  battle,
 };
